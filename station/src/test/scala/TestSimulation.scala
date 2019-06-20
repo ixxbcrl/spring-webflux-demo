4dc314ea1877dcb53f2753111770f7ee13124682
Ming <lxm2133@yahoo.com>
2019-06-21 01:00:17 +0800

    Consolidated test scenarios

--- station/src/test/scala/TestSimulation.scala
+++ station/src/test/scala/TestSimulation.scala
@@ -8,22 +8,36 @@ class TestSimulation extends Simulation {
     .baseUrl("http://localhost:8080")
     .acceptHeader("application/json;charset=UTF-8")
 
-  val scn = scenario("TestSimulation")
-    .exec(http("request_1")
-      .get(URI).queryParam("delay", 20).check(status.is(200)))
-    .pause(5)
+  val httpProtocolBlocking = http
+    .baseUrl("http://localhost:8082")
+    .acceptHeader("application/json;charset=UTF-8")
 
-  val repeatScn = scenario("LoadSimulation").exec(
-    repeat(30) {
-      exec(
-        http("request_2")
-          .get(URI).queryParam("delay", 20).check(status.is(200)))
-        .pause(5)
-    }
+  val scn1 = scenario("TestSimulation")
+    .exec(
+      repeat(10) {
+        exec(
+          http("reactive")
+            .get(URI).queryParam("delay", 20).check(status.is(200)))
+          .pause(5)
+      }
+    )
+
+
+  val scn2 = scenario("LoadSimulation")
+    .exec(
+      repeat(10) {
+        exec(
+          http("blocking")
+            .get(URI).queryParam("delay", 20).check(status.is(200)))
+          .pause(5)
+      }
   )
 
   setUp(
-    scn.inject(atOnceUsers(1))
-  ).protocols(httpProtocol)
+    scn1.inject(atOnceUsers(1000))
+      .protocols(httpProtocol),
+    scn2.inject(atOnceUsers(1000))
+      .protocols(httpProtocolBlocking)
+  )
 
 }
